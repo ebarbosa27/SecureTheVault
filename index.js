67dce4f7a1ed8b4ef448df9b5bb46c05f9edebec
ebarbosa27 <ebarbosa5449@gmail.com>
2025-04-28 21:20:18 -0500

    added index.js script file with secret information of the vault code

---
+++ index.js
@@ -0,0 +1,29 @@
+// Step 1
+let code1;
+
+// Step 2
+code1 = 7 + 3;
+
+// Step 3
+const code2 = 150 / 3 - 10;
+
+// Step 4
+const code3 = 819 % 60;
+
+// Step 5
+const message = "The vault has been secured. The combination is:";
+
+// Step 6
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+// Step 7
+const codeB = `${code1}-${code2}-${code3}`;
+
+// Step 8
+console.log(message);
+console.log(codeA);
+console.log(codeB);
+
+// console.log(code1);
+// console.log(code2);
+// console.log(code3);
